1ff4017b1ca209c5c5862ee2c170422190acf923
unknown <marat76593@gmail.com>
2024-10-05 23:11:50 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+  const pathToFile = path.join('.', 'fs', 'files', 'fresh.txt');
+  try {
+    await fs.access(pathToFile);
+  } catch (error) {
+    const content = `I am fresh and young`;
+    await fs.writeFile(pathToFile, content);
+    return;
+  }
+  console.log(1);
+  throw new Error(`FS operation failed`);
+
 };
 
 await create();
