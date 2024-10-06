4b8353f7491de18ed21366fcd6675de5ccf65070
unknown <marat76593@gmail.com>
2024-10-07 00:59:15 +0300

    feat: list.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.join(import.meta.dirname, 'files', 'fileToRemove.txt');
+  try {
+    await fs.unlink(pathToFile);
+  } catch (error) {
+    throw new Error(`FS operation failed`);
+  }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFiles = path.join(import.meta.dirname, 'files');
+  try {
+    const files = await fs.readdir(pathToFiles);
+    console.log(files);
+  } catch (error) {
+    throw new Error(`FS operation failed`);
+  }
 };
 
 await list();
