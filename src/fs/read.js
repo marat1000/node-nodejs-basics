ffa72842cce9214db1691a31a5f1a763da1087b4
unknown <marat76593@gmail.com>
2024-10-07 01:12:00 +0300

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+  try {
+    const content = await fs.readFile(pathToFile, {encoding: 'utf8'});
+    console.log(content);
+  } catch (error) {
+    throw new Error(`FS operation failed`);
+  }
 };
 
 await read();
