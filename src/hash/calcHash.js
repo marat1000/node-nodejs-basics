58354fd0c9094f950cde706c064836c66490c589
unknown <marat76593@gmail.com>
2024-10-08 01:08:59 +0300

    feat: read.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.join(import.meta.dirname, 'files', `fileToCalculateHashFor.txt`);
+
+  try {
+    const hashValue = await getHash(pathToFile);
+    console.log(hashValue);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+  function getHash (path) {
+    return new Promise((resolve, reject) => {
+      const hash = crypto.createHash('sha256');
+      const rs = fs.createReadStream(path);
+      rs.on('error', reject);
+      rs.on('data', chunk => {
+        hash.update(chunk);
+      });
+      rs.on('end', () => resolve(hash.digest('hex')));
+    })
+  }
 };
 
 await calculateHash();
